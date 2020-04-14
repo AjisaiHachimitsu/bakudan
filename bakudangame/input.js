81c1f1f53d753a9a77c80032295beae700a4ef2a
daigan <daigan47@gmail.com>
2020-04-14 22:59:52 +0900

    input static

--- bakudangame/input.js
+++ bakudangame/input.js
@@ -1,31 +1,31 @@
 import PlayerControler from "./player_controler.js";
 import { Direction } from "./player.js";
-let inputTable;
-function start(inputTable0) {
-    inputTable = inputTable0;
-    for (let i = 0; i < 3; i++) {
-        inputTable.insertRow();
-        for (let j = 0; j < 3; j++) {
-            inputTable.rows[i].insertCell();
+export default class Input {
+    static start(inputTable0) {
+        this.inputTable = inputTable0;
+        for (let i = 0; i < 3; i++) {
+            this.inputTable.insertRow();
+            for (let j = 0; j < 3; j++) {
+                this.inputTable.rows[i].insertCell();
+            }
         }
+        let getCell = (i, j) => {
+            return this.inputTable.rows[i].cells[j];
+        };
+        getCell(0, 1).innerHTML = '<button id="up-button">↑</button>';
+        let upButton = document.getElementById("up-button");
+        upButton.onclick = function () { PlayerControler.ArrowButtonClick(Direction.TOUP); };
+        getCell(1, 0).innerHTML = '<button id="left-button">←</button>';
+        let leftButton = document.getElementById("left-button");
+        leftButton.onclick = function () { PlayerControler.ArrowButtonClick(Direction.TOLEFT); };
+        getCell(1, 2).innerHTML = '<button id="right-button">→</button>';
+        let rightButton = document.getElementById("right-button");
+        rightButton.onclick = function () { PlayerControler.ArrowButtonClick(Direction.TORIGHT); };
+        getCell(2, 1).innerHTML = '<button id="down-button">↓</button>';
+        let downButton = document.getElementById("down-button");
+        downButton.onclick = function () { PlayerControler.ArrowButtonClick(Direction.TODOWN); };
+        getCell(1, 1).innerHTML = '<button>Bomb!</button>';
+        getCell(2, 2).innerHTML = '<button>Pass</button>';
     }
-    let getCell = (i, j) => {
-        return inputTable.rows[i].cells[j];
-    };
-    getCell(0, 1).innerHTML = '<button id="up-button">↑</button>';
-    let upButton = document.getElementById("up-button");
-    upButton.onclick = function () { PlayerControler.ArrowButtonClick(Direction.TOUP); };
-    getCell(1, 0).innerHTML = '<button id="left-button">←</button>';
-    let leftButton = document.getElementById("left-button");
-    leftButton.onclick = function () { PlayerControler.ArrowButtonClick(Direction.TOLEFT); };
-    getCell(1, 2).innerHTML = '<button id="right-button">→</button>';
-    let rightButton = document.getElementById("right-button");
-    rightButton.onclick = function () { PlayerControler.ArrowButtonClick(Direction.TORIGHT); };
-    getCell(2, 1).innerHTML = '<button id="down-button">↓</button>';
-    let downButton = document.getElementById("down-button");
-    downButton.onclick = function () { PlayerControler.ArrowButtonClick(Direction.TODOWN); };
-    getCell(1, 1).innerHTML = '<button>Bomb!</button>';
-    getCell(2, 2).innerHTML = '<button>Pass</button>';
 }
-export default { start };
 //# sourceMappingURL=input.js.map
--- bakudangame/input.ts
+++ bakudangame/input.ts
@@ -1,36 +1,37 @@
 ﻿import PlayerControler from "./player_controler.js";
 import { Direction } from "./player.js";
 
-
-let inputTable: HTMLTableElement;
-function start(inputTable0: HTMLTableElement)
+export default class Input 
 {
-    inputTable = inputTable0;
-    for (let i = 0; i < 3; i++)
+        private static inputTable: HTMLTableElement;
+    static start(inputTable0: HTMLTableElement)
     {
-        inputTable.insertRow();
-        for (let j = 0; j < 3; j++)
+        this.inputTable = inputTable0;
+        for (let i = 0; i < 3; i++)
         {
-            inputTable.rows[i].insertCell();
+            this.inputTable.insertRow();
+            for (let j = 0; j < 3; j++)
+            {
+                this.inputTable.rows[i].insertCell();
+            }
         }
+        let getCell = (i: number, j: number) =>
+        {
+            return this.inputTable.rows[i].cells[j];
+        }
+        getCell(0, 1).innerHTML = '<button id="up-button">↑</button>';
+        let upButton = document.getElementById("up-button") as HTMLButtonElement;
+        upButton.onclick = function () { PlayerControler.ArrowButtonClick(Direction.TOUP); };
+        getCell(1, 0).innerHTML = '<button id="left-button">←</button>';
+        let leftButton = document.getElementById("left-button") as HTMLButtonElement;
+        leftButton.onclick = function () { PlayerControler.ArrowButtonClick(Direction.TOLEFT); };
+        getCell(1, 2).innerHTML = '<button id="right-button">→</button>';
+        let rightButton = document.getElementById("right-button") as HTMLButtonElement;
+        rightButton.onclick = function () { PlayerControler.ArrowButtonClick(Direction.TORIGHT); };
+        getCell(2, 1).innerHTML = '<button id="down-button">↓</button>';
+        let downButton = document.getElementById("down-button") as HTMLButtonElement;
+        downButton.onclick = function () { PlayerControler.ArrowButtonClick(Direction.TODOWN); };
+        getCell(1, 1).innerHTML = '<button>Bomb!</button>';
+        getCell(2, 2).innerHTML = '<button>Pass</button>';
     }
-    let getCell = (i: number, j: number) =>
-    {
-        return inputTable.rows[i].cells[j];
-    }
-    getCell(0, 1).innerHTML = '<button id="up-button">↑</button>';
-    let upButton = document.getElementById("up-button") as HTMLButtonElement;
-    upButton.onclick = function () { PlayerControler.ArrowButtonClick(Direction.TOUP); };
-    getCell(1, 0).innerHTML = '<button id="left-button">←</button>';
-    let leftButton = document.getElementById("left-button") as HTMLButtonElement;
-    leftButton.onclick = function () { PlayerControler.ArrowButtonClick(Direction.TOLEFT); };
-    getCell(1, 2).innerHTML = '<button id="right-button">→</button>';
-    let rightButton = document.getElementById("right-button") as HTMLButtonElement;
-    rightButton.onclick = function () { PlayerControler.ArrowButtonClick(Direction.TORIGHT); };
-    getCell(2, 1).innerHTML = '<button id="down-button">↓</button>';
-    let downButton = document.getElementById("down-button") as HTMLButtonElement;
-    downButton.onclick = function () { PlayerControler.ArrowButtonClick(Direction.TODOWN); };
-    getCell(1, 1).innerHTML = '<button>Bomb!</button>';
-    getCell(2, 2).innerHTML = '<button>Pass</button>';
 }
-export default { start };
