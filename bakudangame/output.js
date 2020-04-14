e8c6b98a325db26b4787ab96d7a68b21b12ead67
daigan <daigan47@gmail.com>
2020-04-14 23:02:55 +0900

    output static

--- bakudangame/output.js
+++ bakudangame/output.js
@@ -1,34 +1,32 @@
 import { fieldStatus } from "./field.js";
-let table;
-let blockCollor;
-let fieldColor;
-function start(table0) {
-    table = table0;
-    blockCollor = "gray";
-    fieldColor = "lightgreen";
-}
-function FieldDraw(field) {
-    table.innerHTML = "";
-    for (let i = 0; i < field.height; i++) {
-        table.insertRow();
-        for (let j = 0; j < field.width; j++) {
-            let cell = table.rows[i].insertCell();
-            if (field.GetField(i, j) == fieldStatus.BLOCK)
-                cell.style.backgroundColor = blockCollor;
-            else
-                cell.style.backgroundColor = fieldColor;
+export default class Output {
+    static start(table0) {
+        this.table = table0;
+        this.blockCollor = "gray";
+        this.fieldColor = "lightgreen";
+    }
+    static FieldDraw(field) {
+        this.table.innerHTML = "";
+        for (let i = 0; i < field.height; i++) {
+            this.table.insertRow();
+            for (let j = 0; j < field.width; j++) {
+                let cell = this.table.rows[i].insertCell();
+                if (field.GetField(i, j) == fieldStatus.BLOCK)
+                    cell.style.backgroundColor = this.blockCollor;
+                else
+                    cell.style.backgroundColor = this.fieldColor;
+            }
         }
     }
+    static PlayerDraw(player) {
+        this.AccessCell(player.y, player.x).innerHTML = '<img src="' + player.imagePath + '"/>';
+    }
+    static AccessCell(i, j) {
+        return this.table.rows[i].cells[j];
+    }
+    static Draw(field, player) {
+        this.FieldDraw(field);
+        this.PlayerDraw(player);
+    }
 }
-function PlayerDraw(player) {
-    AccessCell(player.y, player.x).innerHTML = '<img src="' + player.imagePath + '"/>';
-}
-function AccessCell(i, j) {
-    return table.rows[i].cells[j];
-}
-function Draw(field, player) {
-    FieldDraw(field);
-    PlayerDraw(player);
-}
-export default { start, Draw };
 //# sourceMappingURL=output.js.map
--- bakudangame/output.ts
+++ bakudangame/output.ts
@@ -1,44 +1,45 @@
 ﻿import { fieldStatus, default as Field } from "./field.js"
 import Player from "./player.js";
 
-
-let table: HTMLTableElement;
-let blockCollor: string;
-let fieldColor: string;
-function start(table0: HTMLTableElement)
+export default class Output
 {
-    table = table0;
-    blockCollor = "gray";
-    fieldColor = "lightgreen";
-}
-
-function FieldDraw(field: Field): void
-{
-    table.innerHTML = "";
-    for (let i = 0; i < field.height; i++)
+        private static table: HTMLTableElement;
+    private static blockCollor: string;
+    private static fieldColor: string;
+    static start(table0: HTMLTableElement)
     {
-        table.insertRow();
-        for (let j = 0; j < field.width; j++)
+        this.table = table0;
+        this.blockCollor = "gray";
+        this.fieldColor = "lightgreen";
+    }
+
+    static FieldDraw(field: Field): void
+    {
+        this.table.innerHTML = "";
+        for (let i = 0; i < field.height; i++)
         {
-            let cell = table.rows[i].insertCell();
-            if (field.GetField(i, j) == fieldStatus.BLOCK)
-                cell.style.backgroundColor = blockCollor;
-            else
-                cell.style.backgroundColor = fieldColor;
+            this.table.insertRow();
+            for (let j = 0; j < field.width; j++)
+            {
+                let cell = this.table.rows[i].insertCell();
+                if (field.GetField(i, j) == fieldStatus.BLOCK)
+                    cell.style.backgroundColor = this.blockCollor;
+                else
+                    cell.style.backgroundColor = this.fieldColor;
+            }
         }
     }
+    static PlayerDraw(player: Player): void
+    {
+        this.AccessCell(player.y, player.x).innerHTML = '<img src="' + player.imagePath + '"/>';
+    }
+    static AccessCell(i: number, j: number): HTMLTableCellElement
+    {
+        return this.table.rows[i].cells[j];
+    }
+    static Draw(field: Field, player: Player)
+    {
+        this.FieldDraw(field);
+        this.PlayerDraw(player)
+    }
 }
-function PlayerDraw(player: Player): void
-{
-    AccessCell(player.y, player.x).innerHTML = '<img src="' + player.imagePath + '"/>';
-}
-function AccessCell(i: number, j: number): HTMLTableCellElement
-{
-    return table.rows[i].cells[j];
-}
-function Draw(field:Field,player:Player)
-{
-    FieldDraw(field);
-    PlayerDraw(player)
-}
-export default { start,Draw };
