f52579420cd6ac68b2699c7f20489a99a23e98d5
daigan <daigan47@gmail.com>
2020-04-18 00:47:29 +0900

    デバッグコード除去

--- bakudangame/app.js
+++ bakudangame/app.js
@@ -1,25 +1,16 @@
 import Field from "./field.js";
+import Output from "./output.js";
+import Input from "./input.js";
 import Messge from "./message.js";
-import List from "./list.js";
+import PlayerControler from "./player_controler.js";
 function main() {
 }
 window.onload = () => {
     let field = new Field(21, 15);
     Messge.start(document.getElementById("message"));
-    /*Output.start((document.getElementById("maintable") as HTMLTableElement));
+    Output.start(document.getElementById("maintable"));
     PlayerControler.start(field);
-    Input.start((document.getElementById("buttontable")) as HTMLTableElement);*/
-    let testlist = new List();
-    for (let i = 0; i < 10; i++) {
-        testlist.add(i);
-    }
-    while (testlist.IsNull == false) {
-        if (testlist.Value === 3)
-            testlist.delete();
-        Messge.AddMessage(String(testlist.Value));
-        Messge.BeginNewLine();
-        testlist.Next();
-    }
+    Input.start((document.getElementById("buttontable")));
 };
 //Output.PlayerDraw(PlayerControler.players[0]);
 //# sourceMappingURL=app.js.map
--- bakudangame/app.ts
+++ bakudangame/app.ts
@@ -13,22 +13,11 @@ window.onload = () =>
 
     let field = new Field(21, 15);
     Messge.start(document.getElementById("message"));
-    /*Output.start((document.getElementById("maintable") as HTMLTableElement));
+    Output.start((document.getElementById("maintable") as HTMLTableElement));
     PlayerControler.start(field);
-    Input.start((document.getElementById("buttontable")) as HTMLTableElement);*/
+    Input.start((document.getElementById("buttontable")) as HTMLTableElement);
 
-    let testlist = new List<number>();
-    for (let i = 0; i < 10; i++)
-    {
-        testlist.add(i);
-    }
-    while (testlist.IsNull == false)
-    {
-        if (testlist.Value === 3) testlist.delete();
-        Messge.AddMessage(String(testlist.Value));
-        Messge.BeginNewLine();
-        testlist.Next();
-    }
+    
 
 };
 
