994b64feb57d256e125ae0568ef0561c6f4e4269
daigan <daigan47@gmail.com>
2020-04-21 09:41:33 +0900

    cpuクラス作成

--- bakudangame/cpu.js
+++ bakudangame/cpu.js
@@ -1,5 +1,10 @@
 export default class Cpu {
     ExploreAllAction(turnPlayer, field) {
+        let actions = new Array(5);
+        for (let i = 0; i < 4; i++) {
+            actions[i] = function () { return turnPlayer.move(i, field); };
+        }
+        //actions[length - 1] = function () {return turnPlayer.PutBomb(field,)}
     }
 }
 //# sourceMappingURL=cpu.js.map
--- bakudangame/cpu.ts
+++ bakudangame/cpu.ts
@@ -1,10 +1,15 @@
-﻿import Player from "./player.js";
+﻿import Player, { Direction } from "./player.js";
 import Field from "./field.js";
 
 export default class Cpu
 {
     ExploreAllAction(turnPlayer: Player, field: Field)
     {
-
+        let actions = new Array<() => boolean>(5);
+        for (let i = 0; i < 4; i++)
+        {
+            actions[i] = function () {return turnPlayer.move(i, field) };
+        }
+        //actions[length - 1] = function () {return turnPlayer.PutBomb(field,)}
     }
 }
