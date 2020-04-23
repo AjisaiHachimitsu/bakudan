6614eb0d7d8a08969be0fa881de2249985b69d69
daigan <daigan47@gmail.com>
2020-04-23 16:58:40 +0900

    調整中

--- bakudangame/cpu.js
+++ bakudangame/cpu.js
@@ -1,3 +1,19 @@
+class TreenodeWithAction {
+    constructor(treenode, action) {
+        this.treenode = treenode;
+        this.action = action;
+    }
+}
+class GameTreeNode {
+    constructor(playerControler, bombControler, field) {
+        this.playerControler = playerControler;
+        this.bombControler = bombControler;
+        this.field = field;
+    }
+    Copy() {
+        return new GameTreeNode(this.playerControler.Copy(), this.bombControler.Copy(), this.field.Copy());
+    }
+}
 export default class Cpu {
     //private allActions: (() => boolean)[][];
     constructor(numOfAction, playerControler, bombControler, field) {
@@ -8,24 +24,38 @@ export default class Cpu {
         this.actionSet = [];
         this.checkActionSet = [];
         for (let i = 0; i < 4; i++) {
-            this.actionSet[i] = (player, field) => { return player.Move(i, field); };
-            this.checkActionSet[i] = (player, field) => { return player.CheckMove(i, field); };
+            this.actionSet[i] = (gameTreeNode) => { return gameTreeNode.playerControler.TurnPlayer.Move(i, gameTreeNode.field); };
+            this.checkActionSet[i] = (gameTreeNode) => { return gameTreeNode.playerControler.TurnPlayer.CheckMove(i, gameTreeNode.field); };
         }
-        this.actionSet.push((player, field, bombControler) => { return player.PutBomb(field, bombControler); });
-        this.checkActionSet.push((player, field, bombControler) => { return player.CheckPutBomb(field, bombControler); });
+        this.actionSet.push((gameTreeNode) => {
+            return gameTreeNode.playerControler.TurnPlayer.PutBomb(gameTreeNode.field, gameTreeNode.bombControler);
+        });
+        this.checkActionSet.push((gameTreeNode) => {
+            return gameTreeNode.playerControler.TurnPlayer.CheckPutBomb(gameTreeNode.field, gameTreeNode.bombControler);
+        });
     }
-    RandomActions(player, field, bombControler) {
-        //let actions: ((player:Player) => boolean)[] = [];
-        for (let i = 0; i < this.numOfAction; i++) {
-            let a;
-            do {
-                if (Math.random() < 0.05)
-                    return;
-                a = Math.floor(Math.random() * this.actionSet.length);
-            } while (this.checkActionSet[a](this.playerControler.TurnPlayer, this.field, this.bombControler) === false);
-            this.actionSet[a](this.playerControler.TurnPlayer, this.field, this.bombControler);
-            this.actionSet[a](player, field, bombControler);
+    CreateActionTree(treeNodeWithAction, depth) {
+        let array = [];
+        for (let i = 0; i < this.actionSet.length; i++) {
+            if (this.checkActionSet[i](treeNodeWithAction.treenode)) {
+                let a = treeNodeWithAction.treenode.Copy();
+                this.actionSet[i](a);
+                if (depth <= 0) {
+                    array.push(new TreenodeWithAction(treeNodeWithAction.treenode, this.actionSet[i]));
+                }
+                else {
+                    let b = this.CreateActionTree(new TreenodeWithAction(a, this.actionSet[i]), depth - 1);
+                    for (let j = 0; j < b.length; j++) {
+                        array.push(b[j]);
+                    }
+                }
+            }
         }
+        array.push(null);
+        return array;
+    }
+    NecstActions() {
+        let tree = new GameTreeNode(this.playerControler, this.bombControler, this.field);
     }
 }
 //# sourceMappingURL=cpu.js.map
--- bakudangame/cpu.ts
+++ bakudangame/cpu.ts
@@ -3,7 +3,35 @@ import Field from "./field.js";
 import PlayerControler from "./player_controler.js";
 import BombControler from "./bomb_controler.js";
 
+class TreenodeWithAction
+{
+    treenode: GameTreeNode;
+    action: (gameTreeNode: GameTreeNode) => boolean;
+    constructor(treenode: GameTreeNode, action: (gameTreeNode: GameTreeNode) => boolean)
+    {
+        this.treenode = treenode;
+        this.action = action;
+    }
+}
+class GameTreeNode
+{
+    playerControler: PlayerControler;
+    bombControler: BombControler;
+    field: Field;
 
+    Copy()
+    {
+        return new GameTreeNode(this.playerControler.Copy(), this.bombControler.Copy(), this.field.Copy());
+
+    }
+    constructor(playerControler: PlayerControler, bombControler: BombControler, field: Field)
+    {
+        this.playerControler = playerControler;
+        this.bombControler = bombControler;
+        this.field = field;
+    }
+
+}
 
 export default class Cpu
 {
@@ -11,11 +39,11 @@ export default class Cpu
     private playerControler: PlayerControler;
     private bombControler: BombControler;
     private field: Field;
-    private actionSet: ((player:Player,field:Field,bombControler?:BombControler)=>boolean)[];
-    private checkActionSet: ((player: Player, field: Field, bombControler?: BombControler) => boolean)[];
+    private actionSet: ((gameTreeNode: GameTreeNode) => boolean)[];
+    private checkActionSet: ((gameTreeNode: Readonly<GameTreeNode>) => boolean)[];
     //private allActions: (() => boolean)[][];
-    
-    
+
+
     constructor(numOfAction: number, playerControler: Readonly<PlayerControler>, bombControler: Readonly<BombControler>, field: Readonly<Field>)
     {
         this.numOfAction = numOfAction;
@@ -28,27 +56,66 @@ export default class Cpu
         this.checkActionSet = [];
         for (let i = 0; i < 4; i++)
         {
-            this.actionSet[i] = (player: Player, field: Field) => { return player.Move(i, field) };
-            this.checkActionSet[i] = (player: Player, field: Field) => { return player.CheckMove(i, field) };
+            this.actionSet[i] = (gameTreeNode: GameTreeNode) => { return gameTreeNode.playerControler.TurnPlayer.Move(i, gameTreeNode.field) };
+            this.checkActionSet[i] = (gameTreeNode: Readonly<GameTreeNode>) => { return gameTreeNode.playerControler.TurnPlayer.CheckMove(i, gameTreeNode.field) };
         }
-        this.actionSet.push((player: Player, field: Field, bombControler: BombControler) => { return player.PutBomb(field, bombControler) })
-        this.checkActionSet.push((player: Player, field: Field, bombControler: BombControler) => { return player.CheckPutBomb(field, bombControler) });
-
-    }
-    RandomActions(player:Player,field:Field,bombControler:BombControler)
-    {
-        //let actions: ((player:Player) => boolean)[] = [];
-        for (let i = 0; i < this.numOfAction; i++)
+        this.actionSet.push((gameTreeNode: GameTreeNode) =>
         {
-            let a:number
-            do
-            {
-                if (Math.random() < 0.05) return;
-                a=Math.floor(Math.random()*this.actionSet.length)
-            } while (this.checkActionSet[a](this.playerControler.TurnPlayer,this.field,this.bombControler) === false)
-            this.actionSet[a](this.playerControler.TurnPlayer, this.field, this.bombControler)
-            this.actionSet[a](player, field, bombControler);
-        }
+            return gameTreeNode.playerControler.TurnPlayer.PutBomb(gameTreeNode.field, gameTreeNode.bombControler)
+        })
+        this.checkActionSet.push((gameTreeNode: Readonly<GameTreeNode>) =>
+        {
+            return gameTreeNode.playerControler.TurnPlayer.CheckPutBomb(gameTreeNode.field, gameTreeNode.bombControler)
+        });
 
     }
+
+    private CreateActionTree(treeNodeWithAction: TreenodeWithAction, depth: number) : TreenodeWithAction[]
+    {
+        let array: TreenodeWithAction[] = [];
+        for (let i = 0; i < this.actionSet.length; i++)
+        {
+            if (this.checkActionSet[i](treeNodeWithAction.treenode))
+            {
+                let a = treeNodeWithAction.treenode.Copy();
+                this.actionSet[i](a)
+                if (depth <= 0)
+                {
+                    array.push(new TreenodeWithAction(treeNodeWithAction.treenode, this.actionSet[i]))
+                }
+                else
+                {
+                    let b = this.CreateActionTree(new TreenodeWithAction(a, this.actionSet[i]), depth - 1)
+                    for (let j = 0; j < b.length; j++)
+                    {
+                        array.push(b[j])
+                    }
+                }
+            }
+        }
+        array.push(null)
+        return array;
+
+    }
+    private NecstActions()
+    {
+        let tree = new GameTreeNode(this.playerControler, this.bombControler, this.field);
+    }
+
+    //RandomActions(player: Player, field: Field, bombControler: BombControler)
+    //{
+    //    //let actions: ((player:Player) => boolean)[] = [];
+    //    for (let i = 0; i < this.numOfAction; i++)
+    //    {
+    //        let a: number
+    //        do
+    //        {
+    //            if (Math.random() < 0.05) return;
+    //            a = Math.floor(Math.random() * this.actionSet.length)
+    //        } while (this.checkActionSet[a](this.playerControler.TurnPlayer, this.field, this.bombControler) === false)
+    //        this.actionSet[a](this.playerControler.TurnPlayer, this.field, this.bombControler)
+    //        this.actionSet[a](player, field, bombControler);
+    //    }
+
+    //}
 }
